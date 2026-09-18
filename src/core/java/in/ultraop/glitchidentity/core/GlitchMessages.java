12d3b6ed6b11440238a2b71b767a01f820ad101a
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-19 01:11:21 +0530

    feat: add initial GlitchIdentity core Fabric and Paper implementation

---
+++ src/core/java/in/ultraop/glitchidentity/core/GlitchMessages.java
@@ -0,0 +1,7 @@
+package in.ultraop.glitchidentity.core;
+
+public final class GlitchMessages {
+    private GlitchMessages() {}
+    public static final String PREFIX = "§8[§dGlitch§8] §r";
+    public static final String HELP = PREFIX + "/glitch add <player> §7| §fremove <player> §7| §flist §7| §freload §7| §fhelp";
+}
