5322a64e65bac331e0b31575dd52f8736f8ef76f
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-19 01:36:20 +0530

    fix: hide glitch identities on both death directions

--- src/fabric/java/in/ultraop/glitchidentity/fabric/FabricNetwork.java
+++ src/fabric/java/in/ultraop/glitchidentity/fabric/FabricNetwork.java
@@ -4,27 +4,59 @@ import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
-import net.minecraft.util.Identifier;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
 
 public final class FabricNetwork {
     public static final Identifier ID = Identifier.of("glitchidentity", "glitch");
     public static final CustomPayload.Type<GlitchPayload> GLITCH = new CustomPayload.Type<>(ID);
 
-    public record GlitchPayload(String victim) implements CustomPayload {
-        public static final com.mojang.serialization.Codec<GlitchPayload> CODEC = null;
+    public record GlitchPayload(
+        String victim,
+        String killer,
+        boolean glitchVictim,
+        boolean glitchKiller
+    ) implements CustomPayload {
         public static final net.minecraft.network.codec.StreamCodec<net.minecraft.network.RegistryByteBuf, GlitchPayload> STREAM_CODEC =
-            net.minecraft.network.codec.StreamCodec.composite(PacketCodecs.STRING, GlitchPayload::victim, GlitchPayload::new);
-        @Override public CustomPayload.Type<? extends CustomPayload> getType() { return GLITCH; }
+            net.minecraft.network.codec.StreamCodec.composite(
+                PacketCodecs.STRING, GlitchPayload::victim,
+                PacketCodecs.STRING, GlitchPayload::killer,
+                PacketCodecs.BOOL, GlitchPayload::glitchVictim,
+                PacketCodecs.BOOL, GlitchPayload::glitchKiller,
+                GlitchPayload::new
+            );
+
+        @Override
+        public CustomPayload.Type<? extends CustomPayload> getType() {
+            return GLITCH;
+        }
     }
 
     static {
         PayloadTypeRegistry.playS2C().register(GLITCH, GlitchPayload.STREAM_CODEC);
     }
 
-    public static void sendGlitch(net.minecraft.server.world.ServerWorld world, ServerPlayerEntity victim) {
-        for (ServerPlayerEntity p : world.getServer().getPlayerManager().getPlayerList()) {
-            if (ServerPlayNetworking.canSend(p, GLITCH)) ServerPlayNetworking.send(p, new GlitchPayload(victim.getName().getString()));
+    public static void sendGlitch(
+        net.minecraft.server.world.ServerWorld world,
+        ServerPlayerEntity victim,
+        ServerPlayerEntity killer,
+        boolean glitchVictim,
+        boolean glitchKiller
+    ) {
+        String safeVictim = glitchVictim ? "" : victim.getName().getString();
+        String safeKiller = killer == null || glitchKiller ? "" : killer.getName().getString();
+
+        GlitchPayload payload = new GlitchPayload(
+            safeVictim,
+            safeKiller,
+            glitchVictim,
+            glitchKiller
+        );
+
+        for (ServerPlayerEntity player : world.getServer().getPlayerManager().getPlayerList()) {
+            if (ServerPlayNetworking.canSend(player, GLITCH)) {
+                ServerPlayNetworking.send(player, payload);
+            }
         }
     }
 }
