2ecbe0bc9fd353c43301fecadba1cc527d46d060
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-19 01:14:11 +0530

    security: remove killer identity from client payload

--- src/fabric/java/in/ultraop/glitchidentity/fabric/FabricNetwork.java
+++ src/fabric/java/in/ultraop/glitchidentity/fabric/FabricNetwork.java
@@ -11,10 +11,10 @@ public final class FabricNetwork {
     public static final Identifier ID = Identifier.of("glitchidentity", "glitch");
     public static final CustomPayload.Type<GlitchPayload> GLITCH = new CustomPayload.Type<>(ID);
 
-    public record GlitchPayload(String victim, String killer) implements CustomPayload {
+    public record GlitchPayload(String victim) implements CustomPayload {
         public static final com.mojang.serialization.Codec<GlitchPayload> CODEC = null;
         public static final net.minecraft.network.codec.StreamCodec<net.minecraft.network.RegistryByteBuf, GlitchPayload> STREAM_CODEC =
-            net.minecraft.network.codec.StreamCodec.composite(PacketCodecs.STRING, GlitchPayload::victim, PacketCodecs.STRING, GlitchPayload::killer, GlitchPayload::new);
+            net.minecraft.network.codec.StreamCodec.composite(PacketCodecs.STRING, GlitchPayload::victim, GlitchPayload::new);
         @Override public CustomPayload.Type<? extends CustomPayload> getType() { return GLITCH; }
     }
 
@@ -22,9 +22,9 @@ public final class FabricNetwork {
         PayloadTypeRegistry.playS2C().register(GLITCH, GlitchPayload.STREAM_CODEC);
     }
 
-    public static void sendGlitch(net.minecraft.server.world.ServerWorld world, ServerPlayerEntity victim, String killer) {
+    public static void sendGlitch(net.minecraft.server.world.ServerWorld world, ServerPlayerEntity victim) {
         for (ServerPlayerEntity p : world.getServer().getPlayerManager().getPlayerList()) {
-            if (ServerPlayNetworking.canSend(p, GLITCH)) ServerPlayNetworking.send(p, new GlitchPayload(victim.getName().getString(), killer));
+            if (ServerPlayNetworking.canSend(p, GLITCH)) ServerPlayNetworking.send(p, new GlitchPayload(victim.getName().getString()));
         }
     }
 }
