71b5002e84eecb23391fb90399c235d4202341f9
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-19 10:20:35 +0530

    fix: correlate glitch payloads to exact death packets

--- src/fabric/java/in/ultraop/glitchidentity/fabric/GlitchPayload.java
+++ src/fabric/java/in/ultraop/glitchidentity/fabric/GlitchPayload.java
@@ -1,9 +1,9 @@
 package in.ultraop.glitchidentity.fabric;
 
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.codec.PacketCodecs;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextCodecs;
@@ -11,6 +11,7 @@ import net.minecraft.util.Identifier;
 
 public record GlitchPayload(
     Text message,
+    int victimEntityId,
     boolean glitchVictim,
     boolean glitchKiller
 ) implements CustomPayload {
@@ -22,6 +23,7 @@ public record GlitchPayload(
     public static final PacketCodec<RegistryByteBuf, GlitchPayload> CODEC =
         PacketCodec.tuple(
             TextCodecs.REGISTRY_PACKET_CODEC, GlitchPayload::message,
+            PacketCodecs.VAR_INT, GlitchPayload::victimEntityId,
             PacketCodecs.BOOLEAN, GlitchPayload::glitchVictim,
             PacketCodecs.BOOLEAN, GlitchPayload::glitchKiller,
             GlitchPayload::new
