105cf618db8014eb0200ae345cefcbc46a9617d0
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-19 10:53:16 +0530

    fix: share Paper glitch markers

---
+++ src/paper/java/in/ultraop/glitchidentity/paper/GlitchIdentityMarkers.java
@@ -0,0 +1,8 @@
+package in.ultraop.glitchidentity.paper;
+
+public final class GlitchIdentityMarkers {
+    public static final String VICTIM_MARKER = "\uE000GIV\uE001";
+    public static final String KILLER_MARKER = "\uE000GIK\uE001";
+
+    private GlitchIdentityMarkers() {}
+}
